f193f9922601eee49bc266995ca8b5e0473c7f96
Compu12 <jdmonge17@gmail.com>
2026-03-20 10:20:48 -0600

    Si

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import React from "react";
 import Home from "./pages/Home";
-<<<<<<< HEAD
 import Footer from "./components/Footer"; 
-=======
-import Footer from "./components/Footer";
-import Admin from "./pages/Admin"
->>>>>>> 862d103ea35a2bc40d1a47d7be36e17c515cf42f
 
 function App(){
   return(
