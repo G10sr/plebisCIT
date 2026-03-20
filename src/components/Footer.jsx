1c8d0434937715c9c31aa83760fe6d9c6a4e4af6
Compu5 <giovanni.sanchez.roman@ctpcit.ed.cr>
2026-03-20 07:45:18 -0600

    react project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import "../assets/css/footer.css";
+
+function Footer(){
+    return(
+        <div id="Footer">
+            <p>Armonía<br /> Todos los derechos reservados<br/>Num: 6110 3229</p>
+        </div>
+    )
+}
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../assets/css/header.css";
+
+function Header() {
+
+    const location = useLocation();
+    const [text, setText] = useState({
+        alt: "Armonía Logo Foto",
+        option1: "Home",
+        option2: "Productos",
+    });
+
+    return (
+        <header>
+            <div id="button-Div">
+                <Link to="/">
+                    <button className={`btn ${location.pathname === "/" ? "active" : ""}`}>{text.option1}</button>
+                </Link>
+                <Link to="/productos">
+                    <button className={`btn ${location.pathname === "/productos" ? "active" : ""}`}>{text.option2}</button>
+                </Link>
+            </div>
+        </header>
+    );
+}
+export default Header;
+
+//contador del carrito en el header
+{}
