ce525ed53ee95797833702dbf601041bfd6a6e02
INNERGAMES <urandarien@gmail.com>
2026-03-26 12:33:50 -0600

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import "../assets/css/Footer.css";
+import logo from "../assets/img/logocut.png";
 
 function Footer(){
     return(
         <div id="Footer">
             
             <p>PlebisCIT©<br /> Todos los derechos reservados<br/></p>
+            <p></p>
+            <img src={logo}/>
         </div>
     )
 }
