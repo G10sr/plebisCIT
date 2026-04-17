23bd633786699c0f241ebe45d1d50cf71640fab6
Computadora5 <example@gmail.com>
2026-04-17 10:33:17 -0600

    changes

--- src/components/VoteList.jsx
+++ src/components/VoteList.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "../assets/css/VoteList.css";
 
 function VoteObject() {
-  // 1. Defined the 'items' array (or you could pass this in as a prop)
   const [items] = useState([
     { id: 1, name: "Option A" },
     { id: 2, name: "Option B" },
@@ -17,12 +16,21 @@ function VoteObject() {
   return (
     <div className="voteListContainer">
       {items.map((product) => (
-        <div 
-          key={product.id} 
-          id={object.id} 
-          className="divObj"
-        >
-          {product.name}
+        <div className="voteObject">
+
+          <div
+            key={product.id} 
+            id={object.id} 
+            className="divObj"
+          >
+            {product.name}
+            <button className="buttonEdit"
+              id={product.id}
+            />
+          </div>
+
+          
+
         </div>
       ))}
 
