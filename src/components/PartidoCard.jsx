ab577564af552e2773cbb0a202803928e8e213c2
jdmonge17-ops <jdmonge17@gmail.com>
2026-04-20 15:18:37 -0600

    Null update!!1!

--- src/components/PartidoCard.jsx
+++ src/components/PartidoCard.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 
-
 function PartidoCard({ partido, onClick, seleccionado }) {
+  const esNulo = partido.id === 0;
+
   return (
     <div
       onClick={onClick}
       style={{
         ...styles.card,
-        border: seleccionado ? "3px solid #2f80ed" : "rgb(211, 211, 211)",
+        border: seleccionado
+          ? "3px solid #2f80ed"
+          : "2px solid rgb(180, 180, 180)",
+        ...(esNulo && styles.nullCard),
       }}
     >
-      <div style={styles.content}>
-        <div style={styles.textContainer}>
-          <h3>{partido.nombre}</h3>
-          <p>{partido.descripcionCorta}</p>
-        </div>
+      {esNulo ? (
+        <h2 style={styles.nullText}>Voto nulo</h2>
+      ) : (
+        <div style={styles.content}>
+          <div style={styles.textContainer}>
+            <h3>{partido.nombre}</h3>
+            <p>{partido.descripcionCorta}</p>
+          </div>
 
-        <div style={styles.imageContainer}>
-          <img src={partido.imagenes[0]} style={styles.image} />
+          <div style={styles.imageContainer}>
+            <img
+              src={partido.imagenes?.[0]}
+              alt={partido.nombre}
+              style={styles.image}
+            />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
-
 
 const styles = {
   card: {
@@ -33,7 +44,21 @@ const styles = {
     width: "100%",
     height: 180,
     cursor: "pointer",
-    border: "2px solid #b4b4b4"
+    border: "2px solid #b4b4b4",
+  },
+
+  // Extra layout for null vote
+  nullCard: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  nullText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#555",
+    textAlign: "center",
   },
 
   content: {
